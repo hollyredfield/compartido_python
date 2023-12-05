ca6800ff4587bc94cae2f656de7b3a074788cff0
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2023-12-05 10:04:26 +0100

    Update lector.py

--- biblioteca_pdd_mysql/lector.py
+++ biblioteca_pdd_mysql/lector.py
@@ -23,36 +23,3 @@ class Lectores:
         return False
     
     
-class LectoresCRUD:
-    def __init__(self, db):
-        self.db = db
-
-    def create_lector(self, lector):
-        try:
-            conexion = self.db.conexion_bdd()
-            cursor = conexion.cursor()
-            query = "INSERT INTO lectores (id_alta, nombre, apellidos, fecha_alta, fecha_baja) VALUES (%s, %s, %s, %s, %s)"
-            values = (lector.id_alta, lector.nombre, lector.apellidos, lector.fecha_alta, lector.fecha_baja)
-            cursor.execute(query, values)
-            conexion.commit()
-            self.db.cerrar_conexion(conexion)
-        except mysql.connector.Error as e:
-            print("Error al crear lector: ", e)
-
-    def read_lector(self, id_alta):
-        try:
-            conexion = self.db.conexion_bdd()
-            cursor = conexion.cursor()
-            query = "SELECT * FROM lectores WHERE id_alta = %s"
-            cursor.execute(query, (id_alta,))
-            lector = cursor.fetchone()
-            self.db.cerrar_conexion(conexion)
-            return lector
-        except mysql.connector.Error as e:
-            print("Error al leer lector: ", e)  
-
-conectar = Conectar()
-crud = LectoresCRUD(conectar)
-lector = Lectores(id_alta=1, nombre='John', apellidos='Doe', fecha_alta='2022-01-01', fecha_baja='2022-12-31')
-crud.create_lector(lector)
-conexion = self.db.conexion_bdd()
