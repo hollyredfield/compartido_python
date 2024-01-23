5b362a78d2faf87f781cd03580572f8e344e9cd8
HollyRedfield <114214711+hollyredfield@users.noreply.github.com>
2024-01-23 13:25:11 +0100

    save

--- SegundoMVC/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'vista/encabezado.php';
-include 'vista/cuerpo.php';
-include 'vista/pie.php';
--- SegundoMVC/vista/encabezado.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Mi Primer MVC</title>
-</head>
-<body>
